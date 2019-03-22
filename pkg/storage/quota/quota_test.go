24f70d2247bffc5d319858eca1f34bd0b833f042
Andrew Werner <ajwerner@cockroachlabs.com>
2019-03-22 12:06:51 -0400

    storage: first pass at a quota pool for reads

---
+++ pkg/storage/quota/quota_test.go
@@ -0,0 +1,35 @@
+package quota
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/cockroachdb/cockroach/pkg/util/leaktest"
+	"github.com/cockroachdb/cockroach/pkg/util/stop"
+)
+
+func TestQuota(t *testing.T) {
+	defer leaktest.AfterTest(t)()
+	ctx := context.Background()
+	stopper := stop.NewStopper()
+	defer stopper.Stop(ctx)
+
+	qp := NewReadQuota(ctx, stopper, Config{})
+	const N = 1000
+	quotas := make([]Quota, N)
+	for i := 0; i < N; i++ {
+		q, err := qp.Acquire(ctx)
+		if err != nil {
+			panic(err)
+		}
+		quotas[i] = q
+	}
+	time.Sleep(time.Second)
+	m := qp.Metrics()
+	fmt.Printf("%+#v %v\n", qp, m.InUse.Value())
+	for i := 0; i < N; i++ {
+		qp.Release(quotas[i], 128*1<<10)
+	}
+}
