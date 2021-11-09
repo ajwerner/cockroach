e2ca023e0fba9d516f2feb1ff7d5a46d4a27a572
Andrew Werner <awerner32@gmail.com>
2021-11-08 21:51:19 -0500

    sql/schemachanger: rework testing knobs to include whole plan
    This change also rearranges some of the dependencies to separate information
    needed during execution from information needed to plan and then execute.
    
    Release note: None

--- pkg/sql/schemachanger/scdeps/exec_deps.go
+++ pkg/sql/schemachanger/scdeps/exec_deps.go
@@ -29,6 +29,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scexec"
 	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scop"
 	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scpb"
+	"github.com/cockroachdb/cockroach/pkg/sql/schemachanger/scrun"
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/cockroach/pkg/util/log/eventpb"
 	"github.com/cockroachdb/errors"
@@ -45,9 +46,7 @@ func NewExecutorDependencies(
 	indexValidator scexec.IndexValidator,
 	cclCallbacks scexec.Partitioner,
 	logEventFn LogEventCallback,
-	testingKnobs *scexec.NewSchemaChangerTestingKnobs,
 	statements []string,
-	phase scop.Phase,
 ) scexec.Dependencies {
 	return &execDeps{
 		txnDeps: txnDeps{
@@ -60,9 +59,7 @@ func NewExecutorDependencies(
 			eventLogWriter:  newEventLogWriter(txn, logEventFn),
 		},
 		indexBackfiller: indexBackfiller,
-		testingKnobs:    testingKnobs,
 		statements:      statements,
-		phase:           phase,
 	}
 }
 
@@ -286,7 +283,6 @@ func (d *txnDeps) SetResumeSpans(
 type execDeps struct {
 	txnDeps
 	indexBackfiller scexec.IndexBackfiller
-	testingKnobs    *scexec.NewSchemaChangerTestingKnobs
 	statements      []string
 	phase           scop.Phase
 }
@@ -323,18 +319,13 @@ func (d *execDeps) TransactionalJobCreator() scexec.TransactionalJobCreator {
 }
 
 // TestingKnobs implements the scexec.Dependencies interface.
-func (d *execDeps) TestingKnobs() *scexec.NewSchemaChangerTestingKnobs {
+func (d *runDeps) TestingKnobs() *scrun.NewSchemaChangerTestingKnobs {
 	return d.testingKnobs
 }
 
 // Statements implements the scexec.Dependencies interface.
 func (d *execDeps) Statements() []string {
 	return d.statements
-}
-
-// Phase implements the scexec.Dependencies interface.
-func (d *execDeps) Phase() scop.Phase {
-	return d.phase
 }
 
 // LogEventCallback call back to allow the new schema changer
